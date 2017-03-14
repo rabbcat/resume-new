aa59a8b0fddb395ec633fc17d4750cd43834bd0c
rabbcat <742062761@qq.com>
2017-03-14 14:13:02 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,30 +3,6 @@ window.onload= function () {
     (function(){
         window.scrollReveal = new scrollReveal({ reset: true, move: '50px'});
     })();
-    //var nav_ul=document.getElementById('nav_ul');
-    //var ullis=nav_ul.getElementsByTagName('li');
-    //var secs=document.getElementsByClassName('section');
-    //var timer=null;
-    //for(var j=0;j<ullis.length;j++) {
-    //    ullis[j].index = j;
-    //    ullis[j].onclick = function () {
-    //        var target = secs[this.index].offsetTop;
-    //        //console.log(target);
-    //        clearInterval(timer);
-    //        timer = setInterval(function () {
-    //            var leader = window.pageYOffset || document.documentElement.offsetTop || document.body.offsetTop || 0;
-    //            console.log(leader);
-    //            var step = (target - leader) / 10;
-    //            step = step > 0 ? Math.ceil(step) : Math.floor(step);
-    //            leader = leader + step;
-    //            window.scrollTo(0,leader);
-    //            if (leader === target) {
-    //                clearInterval(timer);
-    //            }
-    //        }, 40);
-    //    };
-    //}
-
 }
 $(function() {
     $('.to-top').toTop();
@@ -50,24 +26,18 @@ $(function() {
 });
 
     var html = document.getElementsByTagName('html')[0];
-    console.log(html);
     /*取到屏幕的宽度*/
     var width = window.innerWidth;
-    console.log(width);
     /* 640 100  320 50 */
     var fontSize = width/640*100;
-    console.log(fontSize);
     /*设置fontsize*/
     html.style.fontSize = fontSize +'px';
     window.onresize = function(){
         var html = document.getElementsByTagName('html')[0];
-        console.log(html);
         /*取到屏幕的宽度*/
         var width = window.innerWidth;
-        console.log(width);
         /* 640 100  320 50 */
         var fontSize = width/640*100;
-        console.log(fontSize);
         /*设置fontsize*/
         html.style.fontSize = fontSize +'px';
     }
